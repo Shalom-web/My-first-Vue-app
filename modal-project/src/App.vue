765215d0cebbfcbb456499ffa42f50a6581ae98f
Shalom-web <armstrongshalom1@gmail.com>
2022-09-20 21:11:37 +0100

    first commit

---
+++ modal-project/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <h1>{{title}}</h1>
+  <p>Welcome...</p>
+
+<teleport to=".modals" v-if="showModal">
+  <Modal theme="sale" @close="toggleModal">
+    <template v-slot:links>
+      <a href="#">Sign up now</a>
+      <a href="#">More info</a>
+    </template>
+    <h1>Giveaway</h1>
+    <p>Grab your free copy now!</p>
+  </Modal>
+</teleport>
+
+<teleport to=".modals" v-if="showModalTwo">
+  <Modal  @close="toggleModal">
+    <h1>Sign up to the newsletter</h1>
+    <p>For updates and promo codes</p>
+  </Modal>
+</teleport>
+
+<button @click.alt="toggleModal">Open Modal (alt)</button>
+<button @click="toggleModalTwo">Open Modal</button>
+
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+  export default{
+    name: 'App',
+    components: { Modal },
+    data() {
+      return {
+        title: 'Welcome to Vue App',
+        showModal: false,
+        showModalTwo: false
+
+      }
+    },
+    methods: {
+      toggleModal(){
+        this.showModal=!this.showModal
+      },
+      toggleModalTwo(){
+        this.showModalTwo = !this.showModalTwo
+      }
+    }
+  }
+</script>
+
+
+<style>
+header {
+  line-height: 1.5;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+  color: rgb(0, 0, 0);
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale: theme === 'sale'}"> 
+          <slot>Dafault Contents</slot>
+          <div class="actions">
+            <slot name="links"></slot>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['theme'],
+        methods: {
+            closeModal(){
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<style>
+    .modal{
+        width: 500px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+        text-align: center;
+    }
+    .backdrop{
+        top: 150px;
+        position: fixed;
+        background: rgb(227, 223, 223);
+        width: 100%;
+        height: 100%;
+       
+    }
+    .modal h1{
+        color: rgb(0, 0, 0);
+        border: none;
+        padding: 0;
+        font-weight: 700;
+    }
+    .modal p{
+        font-style: normal;
+    }
+    .modal .actions{
+        text-align: center;
+        margin: 30px 0 10px 0;
+        color: #333;
+    }
+    .modal .actions a{
+        color: #333;
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        margin: 10px;
+    }
+    .modal.sale{
+        background: rgb(85, 188, 239);
+        color: rgb(255, 255, 255);
+    }
+    .modal.sale.h1{
+        color: white;
+    }
+    .modal.sale .actions a{
+        color: white;
+    }
+</style>
